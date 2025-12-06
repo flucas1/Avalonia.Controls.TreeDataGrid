1acd86e30186e766a82cfb744b0e90d3d098f02c
Fernando Lucas <info@fernandolucas.info>
2025-12-06 20:18:47 +0100

    public

---
+++ src/Avalonia.Controls.TreeDataGrid/Themes/Generic.axaml.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Avalonia.Styling;
+
+namespace Avalonia.Controls.Themes
+{
+    public class Generic : Style
+    {
+    }
+}
